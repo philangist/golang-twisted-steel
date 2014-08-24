b010b82cda43a4696e4fb58e524cb3e74e240e2e
Phil Opaola <phil.opaola@percolate.com>
2014-08-23 22:39:02 -0400

    s/_/camel

--- api_client/api_client.go
+++ api_client/api_client.go
@@ -5,6 +5,8 @@ import (
   "fmt"
   "http"
   "ioutil"
+  "http"
+  "time"
 )
 
 // Tinder api client only supports get and post requests
@@ -18,59 +20,60 @@ const (
 // http responses, sets auth parameters before every request, and handles
 // boilerplate so that TinderApiClient can just work
 type APIClient struct {
-  base_url string
-  auth_header string
+  baseUrl string
+  fbOauthToken string
+  sessionAuthToken string
+  client  http.Client
 }
 
-func New(base_url string, fb_auth_token string) *APIClient {
+func New(baseUrl string, fb_auth_token string) *APIClient {
   // set a timeout on this client
   client = &http.Client{
     Timeout: time.Duration(HTTP_REQUEST_TIMEOUT)*time.Second
   }
 
   return &APIClient{
-    base_url: base_url,
-    auth_header: auth_header,
-    fb_oauth_token: fb_oauth_token,
-    session_auth_token: nil,
+    baseUrl: baseUrl,
+    fbOauthToken: fbOauthToken,
+    sessionAuthToken: nil,
     client: Client
   }
 }
 
-func (this APIClient) buildQueryParamString(query_params) string {
-  query_params_string := ''
+func (this APIClient) buildQueryParamString(queryParams) string {
+  queryParamsString := ''
 
-  if query_params != nil {
+  if queryParams != nil {
     idx = 0
-    for key, value := range query_params{
+    for key, value := range queryParams{
       if idx == 0:
         key = fmt.Sprintf('?%s', key)
 
-      query_param_string = fmt.Sprintf('%s%s&', key, val)
-      query_params_string += query_param_string
+      queryParamString = fmt.Sprintf('%s%s&', key, val)
+      queryParamsString += queryParamString
       idx += 1
     }
-    query_params_string = query_params_string[:-1]  // strip off trailing '&'
+    queryParamsString = queryParamsString[:-1]  // strip off trailing '&'
   }
 
-  return query_params_string
+  return queryParamsString
 }
 
 func (this APIClient) httpRequest(method string, path string) (map, error){
-  if this.auth_header == nil {
+  if this.sessionAuthToken == nil {
     return nil, (
       "Client can not be used to make requests "
       "unless authenticated session toke is available"
       )
   }
 
-  full_path := this.base_url + path
+  fullPath := this.baseUrl + path
 
   switch method {
     case GET:
-      resp, err := http.Get(full_path) // set headers on request
+      resp, err := http.Get(fullPath) // set headers on request
     case POST:
-      resp, err := http.Post(full_path) // set headers on request
+      resp, err := http.Post(fullPath) // set headers on request
     default:
       return nil, fmt.Sprintf(
         "Only %s and %s are supported by this client", GET, POST)
@@ -86,19 +89,19 @@ func (this APIClient) httpRequest(method string, path string) (map, error){
   if err != nil{
     return nil, err
   }
-  parsed_response, err = json.Unmarshal(data)
+  parsedResponse, err = json.Unmarshal(data)
 
-  return parsed_response, err
+  return parsedResponse, err
 
 }
 
-func (this APIClient) Get(path string, query_params map) (map, error) {
+func (this APIClient) Get(path string, queryParams map) (map, error) {
   // Url encodes params, hits endpoint, returns map{}
-  path =  path + this.buildQueryParamString(query_params)
-  return this.httpRequest(GET, path+query_params_string)
+  path =  path + this.buildQueryParamString(queryParams)
+  return this.httpRequest(GET, path+queryParamsString)
 }
 
-func (this APIClient) Post(path string, query_params map) (map, error) {
-  path =  path + this.buildQueryParamString(query_params)
+func (this APIClient) Post(path string, queryParams map) (map, error) {
+  path =  path + this.buildQueryParamString(queryParams)
   return this.httpRequest(POST, path)
 }
