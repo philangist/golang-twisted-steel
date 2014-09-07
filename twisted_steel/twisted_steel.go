593a53b1c2b780179331dc7aed1bb2258acd1190
Phil Opaola <phil.opaola@percolate.com>
2014-09-07 19:02:40 -0400

    Add instagram api client factory

--- twisted_steel/twisted_steel.go
+++ twisted_steel/twisted_steel.go
@@ -1,35 +1,27 @@
 package main
 
 import (
+  "errors"
   "fmt"
   "net/url"
+  "os"
 
   "philangist.github.com/twisted-steel/api_client"
   "philangist.github.com/restful-micro-framework/sleepy"
   )
 
+const (
+  SECRET_SUFFIX = "_TOKEN"
+  FACEBOOK = "FACEBOOK"
+  INSTAGRAM = "INSTAGRAM"
+  TWITTER = "TWITTER"
+  TUMBLR = "TUMBLR"
+  VINE = "VINE"
+  )
+
 var (
-  ApiClient = api_client.NewApiClient("https://api.instagram.com/v1")
-  AuthParams = map[string]string{
-    "access_token": "SOME_KEY_HERE", //get this from environment
-  }
-  InstagramClient = InstagramApiClient{
-    ApiClient: ApiClient,
-    AuthParams: AuthParams,
-  }
+  InstagramClient = createInstagramClient()
 )
-
-
-type InstagramApiClient struct {
-  ApiClient api_client.APIClient
-  AuthParams map[string]string
-}
-
-type InstagramUserResource struct {
-  sleepy.PostNotSupported
-  sleepy.PutNotSupported
-  sleepy.DeleteNotSupported
-}
 
 func check(err error){
   if err != nil{
@@ -37,20 +29,48 @@ func check(err error){
   }
 }
 
-func (InstagramUserResource) Get(values url.Values) (int, interface{}){
-  var err error
-
-  userId := values.Get("user_id")
-  if userId == "" {
-    return 400, map[string]string{
-      "error": "user_id must be specified",
-      }
+func createInstagramClient() InstagramApiClient{
+  access_token, err := getTokenForPlatform(INSTAGRAM)
+  ApiClient := api_client.NewApiClient("https://api.instagram.com/v1")
+  check(err)
+  AuthParams := map[string]string{
+    "access_token": access_token,
   }
-
-  userData := getInstagramUser(userId)
-  return 200, map[string]string{"data": string(userData[:])}
+  return InstagramApiClient{
+    ApiClient: ApiClient,
+    AuthParams: AuthParams,
+  }
 }
 
+func getTokenForPlatform(PlatformName string) (string, error) {
+  var key, value string
+
+  key = fmt.Sprintf("%s%s", PlatformName, SECRET_SUFFIX)
+  value = os.Getenv(key)
+
+  if value == "" {
+    return "", errors.New(fmt.Sprintf(
+      "Could not get value for %s from environment", key))
+  }
+  return value, nil
+}
+
+type PlatformUser struct{
+  UserName, UserId string
+}
+
+type PlatformApiClient interface {
+  getMe() PlatformUser
+  getFollowers() []PlatformUser
+  getFollowing() []PlatformUser
+  followUser() (bool, error) //accepts PlatformUser as argument
+  unfollowUser() (bool, error) //accepts PlatformUser as argument
+}
+
+type InstagramApiClient struct {
+  ApiClient api_client.APIClient
+  AuthParams map[string]string
+}
 
 func (this InstagramApiClient) GetUser(userId string) ([]byte, error){
   return this.ApiClient.Get(
@@ -67,9 +87,28 @@ func getInstagramUser(userId string) []byte{
 }
 
 
+type InstagramUserResource struct {
+  sleepy.PostNotSupported
+  sleepy.PutNotSupported
+  sleepy.DeleteNotSupported
+}
+
+// api view logic
+
+func (InstagramUserResource) Get(values url.Values) (int, interface{}){
+  userId := values.Get("user_id")
+  if userId == "" {
+    return 400, map[string]string{
+      "error": "user_id must be specified",
+      }
+  }
+
+  userData := getInstagramUser(userId)
+  return 200, map[string]string{"data": string(userData[:])}
+}
+
 func main(){
   instagramResource := new(InstagramUserResource)
-
   var api = new(sleepy.API)
   api.AddResource(instagramResource, "/users/")
   api.Start(3000)
